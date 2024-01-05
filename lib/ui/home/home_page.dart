79f23a7a0282a9d748f70c5beea1006d2a5a1ba5
onishi taisei <taisei.developer@gmail.com>
2024-01-05 21:32:57 +0900

    🚀 feat: 一覧の表示追加

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -19,37 +19,56 @@ class HomePage extends HookConsumerWidget {
 
     logger.i(state);
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Align(
-          child: state.maybeWhen(
-            data: (data) {
-              return Text(data.length.toString());
-            },
-            orElse: SizedBox.shrink,
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Align(
+              child: state.maybeWhen(
+                data: (data) {
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final activity = data[index];
+                      return Column(
+                        children: [
+                          Text(activity.title ?? ''),
+                          Text(activity.description ?? ''),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('削除ボタン'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                orElse: SizedBox.shrink,
+              ),
+            ),
           ),
-        ),
-        const Text('タイトル'),
-        TextFormField(
-          controller: titleController,
-        ),
-        const Text('内容'),
-        TextFormField(
-          controller: descriptionController,
-        ),
-        Align(
-          child: ElevatedButton(
-            onPressed: () =>
-                ref.read(homeNotifierProvider.notifier).addActivity(
-                      title: titleController.text,
-                      description: descriptionController.text,
-                    ),
-            child: const Text('追加'),
+          const Text('タイトル'),
+          TextFormField(
+            controller: titleController,
           ),
-        ),
-      ],
+          const Text('内容'),
+          TextFormField(
+            controller: descriptionController,
+          ),
+          Align(
+            child: ElevatedButton(
+              onPressed: () =>
+                  ref.read(homeNotifierProvider.notifier).addActivity(
+                        title: titleController.text,
+                        description: descriptionController.text,
+                      ),
+              child: const Text('追加'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
